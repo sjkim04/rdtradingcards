5ea1eff62cca9bbc097d57d62819a0f46bc34832
sjk <sungntung@gmail.com>
2022-08-07 21:30:48 +0900

    Add checkcard JSON

--- commands/checkcard.lua
+++ commands/checkcard.lua
@@ -2,11 +2,12 @@ local command = {}
 function command.run(message, mt)
   print(message.author.name .. " did !checkcard")
   local uj = dpf.loadjson("savedata/" .. message.author.id .. ".json",defaultjson)
+  local lang = dpf.loadjson("langs/" .. uj.lang .. "/checkcard.json","")
   uj.checkcard = not uj.checkcard
   if uj.checkcard then
-    message.channel:send("You will no longer be notificated if you pull or box a card that you have none of in your storage!")
+    message.channel:send(lang.disabled_message)
   else
-    message.channel:send("You will now be notificated if you pull or box a card that you have none of in your storage!")
+    message.channel:send(lang.enabled_message)
   end
   dpf.savejson("savedata/" .. message.author.id .. ".json",uj)
 end
