60ef71849cbc857619136efd4a9e76054e1508b0
sjk <sungntung@gmail.com>
2022-08-14 09:44:11 +0900

    Add Korean ping + Finish lab JSONs
    The tracery was too hard to modify, so just a patch for now

--- commands/ping.lua
+++ commands/ping.lua
@@ -1,6 +1,13 @@
 local command = {}
 function command.run(message, mt)
-  message.channel:send(trf('ping'))
+  local uj = dpf.loadjson("savedata/" .. message.author.id .. ".json",defaultjson)
+  if uj.lang == "ko" then
+      local pingmessage = {"핑!", "퐁!", "팽!", "핀!"}
+	  local cping = math.random(1, #pingmessage)
+	  message.channel:send(pingmessage[cping])
+  else
+      message.channel:send(trf('ping'))
+  end
   print(message.author.name .. " did !ping")
 end
 return command
