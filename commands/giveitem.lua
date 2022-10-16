68b4e23cb891e52a269fb24986d373c6103f1a21
sjk <sungntung@gmail.com>
2022-10-16 18:22:27 +0900

    give, giveitem

--- commands/giveitem.lua
+++ commands/giveitem.lua
@@ -33,7 +33,7 @@ function command.run(message, mt)
   end
   
   if not uj2f then
-    message.channel:send(lang.no_user_1 .. user_argument .. lang.no_user_2)
+    message.channel:send(formatstring(lang.no_user, {user_argument}))
     return
   end
 
@@ -58,9 +58,9 @@ function command.run(message, mt)
     if itemtexttofn(thing_argument) then
       message.channel:send(lang.equippable_item)
     elseif nopeeking then
-      message.channel:send(lang.no_item_nopeeking_1 .. thing_argument .. lang.no_item_nopeeking_2)
+      message.channel:send(formatstring(lang.no_item_nopeeking, {thing_argument}))
     else
-      message.channel:send(lang.no_item_1 .. thing_argument .. lang.no_item_2)
+      message.channel:send(formatstring(lang.no_item, {thing_argument}))
     end
     return
   end
@@ -68,16 +68,16 @@ function command.run(message, mt)
   if not uj.consumables[curfilename] then
     print("user doesnt have item")
     if nopeeking then
-      message.channel:send(lang.no_item_nopeeking_1 .. thing_argument .. lang.no_item_nopeeking_2)
+      message.channel:send(formatstring(lang.no_item_nopeeking, {thing_argument}))
     else
-      message.channel:send(lang.dont_have_1 .. consdb[curfilename].name .. lang.dont_have_2)
+      message.channel:send(formatstring(lang.dont_have, {consdb[curfilename].name}))
     end
     return
   end
   
   if not (uj.consumables[curfilename] >= numitems) then
     print("user doesn't have enough items")
-    message.channel:send(lang.not_enough_1 .. consdb[curfilename].name .. lang.not_enough_2)
+    message.channel:send(formatstring(lang.not_enough, {consdb[curfilename].name}))
     return
   end
 
@@ -101,19 +101,9 @@ function command.run(message, mt)
   dpf.savejson(uj2f,uj2)
   print("saved user2 json with new item")
   
-  local isplural = numitems ~= 1 and lang.needs_plural_s == true and "s" or ""
-  local isplural2 = numitems ~= 1 and lang2.needs_plural_s == true and "s" or ""
+  local giftedmessage = formatstring(lang.gifted_message, {numitems, consdb[curfilename].name, uj2.id}, lang.plural_s)
+  local recievedmessage = formatstring(lang2.recieved_message, {uj.id, numitems, consdb[curfilename].name}, lang2.plural_s)
   
-  if uj.lang == "ko" then
-    _G['giftedmessage'] = lang.gifted_message_1 .. uj2.id .. lang.gifted_message_2 .. consdb[curfilename].name .. lang.gifted_message_3 .. numitems .. lang.gifted_message_4 .. lang.gifted_message_5
-  else
-    _G['giftedmessage'] = lang.gifted_message_1 .. numitems .. lang.gifted_message_2 .. consdb[curfilename].name .. lang.gifted_message_3 .. isplural .. lang.gifted_message_4 .. uj2.id .. lang.gifted_message_5
-  end
-  if uj2.lang == "ko" then
-    _G['recievedmessage'] = lang2.recieved_message_1 .. uj.id .. lang2.recieved_message_2 .. consdb[curfilename].name .. lang2.recieved_message_3 .. numitems .. lang2.recieved_message_4 .. lang2.recieved_message_5
-  else
-    _G['recievedmessage'] = lang2.recieved_message_1 .. uj.id .. lang2.recieved_message_2 .. numitems .. lang2.recieved_message_3 .. consdb[curfilename].name .. lang2.recieved_message_4 .. isplural2 .. lang2.recieved_message_5
-  end
   if uj.lang == uj2.lang then
     message.channel:send {
       content = giftedmessage
